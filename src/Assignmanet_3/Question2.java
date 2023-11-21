f2914e2623be3766e125cc2fd0aacfc8750ad48f
John Safwat <106314101+john-safwat@users.noreply.github.com>
2023-11-21 02:13:42 +0200

    update assignment 3

--- src/Assignmanet_3/Question2.java
+++ src/Assignmanet_3/Question2.java
@@ -3,15 +3,30 @@ package Assignmanet_3;
 import java.util.Objects;
 
 public class Question2 {
+
+    public static void main(String[] args) {
+        // Creating a Holiday instance for Independence Day
+        Holiday octoberVictory = new Holiday("October Victory", 6, "October");
+
+        // Example array of Holiday instances
+        Holiday[] holidayArray = {
+                new Holiday("New Year", 1, "January"),
+                new Holiday("Christmas", 25, "December"),
+                new Holiday("Thanksgiving", 26, "November")
+        };
+
+        System.out.println("The Average days = "+Holiday.avgDate(holidayArray));
+    }
+
 }
 
 
 class Holiday {
 
     // define the variables
-    private String name;
-    private int day;
-    private String month;
+    private final String name;
+    private final int day;
+    private final String month;
 
     // the default constructor to give the initial values
     Holiday(){
@@ -27,10 +42,20 @@ class Holiday {
         this.month = !month.isEmpty() ? month : "None";
     }
 
-
     // function inSameMonth to see if the holiday in the same month
+    // this function take value of type holiday and validate in the month of this object and the month in passed holiday
     public Boolean isSameMonth(Holiday holiday) {
         return Objects.equals(this.month, holiday.month);
+    }
+
+    // function to calculate the average day in an array of Holiday instances
+    // this function take array if holiday and loop on this array and sum all days values
+    public static double avgDate(Holiday[] holidays) {
+        int daysSum = 0;
+        for (Holiday holiday : holidays) {
+            daysSum += holiday.day;
+        }
+        return (double) daysSum / holidays.length;
     }
 
 }
